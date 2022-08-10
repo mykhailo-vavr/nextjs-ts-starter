83784e5fb3f52167e67a117bacb95958db2bce9f
mykhailo-vavr <vavr.mykhailo@gmail.com>
2022-08-10 15:59:19 +0300

    update pages folder

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => (
+  <Html>
+    <Head>
+      <meta charSet="utf-8" />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+export default Document
